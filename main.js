0ff038f4e2540284a21285bc94cd39316f623ea0
Elephantino <rybinsky.igor@mail.ru>
2022-11-28 20:48:27 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+$(function () {
+
+  $('.slick').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    adaptiveHeight: true,
+    speed: 300,
+    initialSlide: 1,
+    // autoplay: true,
+    autoplaySpeed: 1000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    draggable: true, // свайп на пк
+    swipe: true,  // свайп на телефоне
+    touchThreshold: 99, // чувствительность свайпа
+    touchMove: true,  // слайдер тянуться не будет
+    waitFodAnimate: true, //быстрое перелистывание слайдов
+    centerMode: false,
+  });
+
+});
+
